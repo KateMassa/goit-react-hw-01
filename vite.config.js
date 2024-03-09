2aea0ad1887af67ff1f4a510fe6305b2a91fe48e
KateMassa <catherineorlova27@gmail.com>
2024-03-09 17:36:29 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,14 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    hmr: {
-      // set MIME type for JSX files
-      mimeTypes: {
-        'text/jsx': ['jsx'],
-      },
-    },
-  },
+  // server: {
+  //   hmr: {
+  //     // set MIME type for JSX files
+  //     mimeTypes: {
+  //       'text/jsx': ['jsx'],
+  //     },
+  //   },
+  // },
   plugins: [react()],
   build: {
     sourcemap: true,
